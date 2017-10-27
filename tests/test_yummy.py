b2848126280127c11b4b186095ebd01bc1030083
atonui <a_tonui@yahoo.com>
2017-10-27 16:17:41 +0300

    updated tests

--- tests/test_yummy.py
+++ tests/test_yummy.py
@@ -1,6 +1,16 @@
+"""script to test users module in app"""
 import unittest
 
 class YummyRecipeTests(unittest.TestCase):
+    """test class for users"""
+
+    def setUp(self):
+        self.new_user = User()
+        self.current_user = userList()
+        self.user_email = "me@me.com"
+        self.username = "me"
+        self.password = "meawww"
+        self.test_user = self.new_user.add_user(self.user_email,self.username,self.password)
 
     def test_user_created(self):
         """test if user has been created"""
