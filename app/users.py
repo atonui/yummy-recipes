9c8d27eb26b37a62b25efc8be2149cf968895ac0
atonui <a_tonui@yahoo.com>
2017-10-28 12:14:04 +0300

    pylint and bug fixes

--- app/users.py
+++ app/users.py
@@ -1,23 +1,23 @@
 """user management script"""
-userList = {}
+USERLIST = {}
 
 class User(object):
-    def __init__(self, email=None,username=None,password=None):
+    def __init__(self, email=None, username=None, password=None):
         self.email = email
         self.username = username
         self.password = password
 
-    def add_user(self,email,username,password):
-        if email not in userList:
-            userList[email] = {"email":email,"username":username,"password":password}
-            return userList
+    def add_user(self, email, username, password):
+        if email not in USERLIST:
+            USERLIST[email] = {"email":email, "username":username, "password":password}
+            return USERLIST
         else:
             return "The email address is already in use."
 
-    def fetch_user(self,email=None,password=None,users=None):
-        if email in userList:
-            current_user = userList[email]
-            if current_user["password"]==password:
+    def fetch_user(self, email=None, password=None, users=None):
+        if email in USERLIST:
+            current_user = USERLIST[email]
+            if current_user["password"] == password:
                 return current_user
             else:
                 return "Non existent user"
--- tests/test_yummy.py
+++ tests/test_yummy.py
@@ -2,23 +2,25 @@
 import unittest
 
 from app.users import User
-from app.users import userList
+from app.users import USERLIST
 
 class YummyRecipeTests(unittest.TestCase):
     """test class for users"""
 
     def setUp(self):
         self.new_user = User()
-        self.current_user = userList
+        self.current_user = USERLIST
         self.user_email = "me@me.com"
         self.username = "me"
         self.password = "meawww"
-        self.test_user = self.new_user.add_user(self.user_email,self.username,self.password)
+        self.test_user = self.new_user.add_user(self.user_email, self.username, self.password)
 
     def test_user_created(self):
         """test if user has been created"""
-        self.assertTrue(self.user_email in self.test_userList.keys())
+        self.assertTrue(self.user_email in self.test_user.keys())
 
     def test_duplicate_user(self):
-        duplicate_user = self.new_user.add_user(self.user_email,"mimi","pass123")
-        self.assertEqual(duplicate_user,"The email is already in use.")
+        """tests for attempt to duplicate email address"""
+        duplicate_user = self.new_user.add_user(self.user_email, "mimi", "pass123")
+        self.assertEqual(duplicate_user, "The email is already in use.")
+        
