fa90a7466b6263f3087b947257c53e41a0b6ffd4
atonui <a_tonui@yahoo.com>
2017-10-28 10:51:05 +0300

    added user modules

--- app/users.py
+++ app/users.py
@@ -6,3 +6,18 @@ class User(object):
         self.email = email
         self.username = username
         self.password = password
+
+    def add_user(self,email,username,password):
+        if email not in userList:
+            userList[email] = {"email":email,"username":username,"password":password}
+            return userList
+        else:
+            return "The email address is already in use."
+
+    def fetch_user(self,email=None,password=None,users=None):
+        if email in userList:
+            current_user = userList[email]
+            if current_user["password"]==password:
+                return current_user
+            else:
+                return "Non existent user"
