a139ce0435593aa17297393638b0bc0c85993afd
atonui <a_tonui@yahoo.com>
2017-11-02 12:09:04 +0300

    [CHORE] fixed test bug

--- tests/test_yummy.py
+++ tests/test_yummy.py
@@ -1,21 +1,22 @@
 """script to test users module in app"""
 import unittest
 
-from app.users import User
+from app.users import Users
 from app.users import USERLIST
 
 class YummyRecipeTests(unittest.TestCase):
     """test class for users"""
 
     def setUp(self):
-        self.new_user = User()
         self.current_user = USERLIST
         self.user_email = "me@me.com"
         self.username = "me"
         self.password = "meawww"
-        self.test_user = self.new_user.add_user(self.user_email, self.username, self.password)
+        self.name = "Ni Mimi"
+        self.new_user = Users(self.user_email, self.password, self.username, self.name)
+        self.test_user = self.new_user.add_user(self.user_email, self.password, self.username, self.name)
 
     def test_user_created(self):
         """test if user has been created"""
-        self.assertTrue(self.user_email in self.test_user.keys())
+        self.assertTrue(self.user_email in self.test_user)
         
